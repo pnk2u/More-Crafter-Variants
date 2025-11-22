ebd810281b00af599ca5995dcae50e9ef266a0d9
pnku <159317511+pnk2u@users.noreply.github.com>
2025-11-23 00:46:23 +0100

    Remove unused packages and resources (1.1.0|4)
    - Removes _Client and _Datagen packages
    - Removes en_gb lang (identical to en_us)

--- src/main/java/de/pnku/mcrv/MoreCrafterVariantsClient.java
+++
@@ -1,9 +0,0 @@
-package de.pnku.mcrv;
-
-import net.fabricmc.api.ClientModInitializer;
-
-public class MoreCrafterVariantsClient implements ClientModInitializer {
-    @Override
-    public void onInitializeClient() {
-    }
-}
