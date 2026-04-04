b21cbfb97cd00c0973dfd3b8dbbc6dc36b81a9d4
pnku <pnku@pnku.de>
2026-04-04 18:28:52 +0200

    Fix CMT registration order (1.1.0-d:c1)

--- src/main/java/de/pnku/mcrv/init/McrvItemInit.java
+++ src/main/java/de/pnku/mcrv/init/McrvItemInit.java
@@ -39,6 +39,6 @@ public class McrvItemInit {
     private static void registerCrafterItem(BlockItem crafter) {
         Registry.register(BuiltInRegistries.ITEM, MoreCrafterVariants.asId(((MoreCrafterBlock) crafter.getBlock()).crafterWoodType + "_crafter"), crafter);
 
-        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FUNCTIONAL_BLOCKS).register(entries -> entries.addAfter(crafter, Items.CRAFTER));
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FUNCTIONAL_BLOCKS).register(entries -> entries.addAfter(Items.CRAFTER, crafter));
     }
 }
